1104cf6372e448af64a1d2ec02286d293c9a159b
PJUAREZC <pedro.juarez@gnp.com.mx>
2019-08-07 18:04:26 -0500

    Calculadora ejercicio

---
+++ src/app/persona/persona.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-persona',
+  templateUrl: './persona.component.html',
+  styleUrls: ['./persona.component.css']
+})
+export class PersonaComponent implements OnInit {
+
+  mensaje: string = "s"; 
+  
+  opA:number; 
+  opB:number; 
+  result:number;
+  constructor() { }
+
+  ngOnInit() {
+  }
+  onModify(event: Event){
+    this.mensaje = (<HTMLInputElement>event.target).value;
+  }
+  sumar(){
+    this.result = Number(this.opA) + Number(this.opB)
+  }
+}
